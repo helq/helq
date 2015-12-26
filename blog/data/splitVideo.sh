20dc7c304192be4c33c8090ec4426d47eb826b64
helq <linuxero789@gmail.com>
2015-12-26 17:26:26 -0500

    2015-12-26T17:28:10-0500

---
+++ blog/data/splitVideo.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 song_name split_time"
+    exit 1
+fi
+
+# name file to split
+name="$1"
+# splitting time in seconds
+splitLen=$2
+
+totalTime="$(ffprobe -i "$name" -show_format 2> /dev/null | sed -n 's/duration=//p')"
+numOfSplits="$(printf '%.0f\n' $[ totalTime / splitLen ])"
+
+for i in {0..$numOfSplits}; do
+    start=$[ splitLen * i ]
+
+    num=$(printf '%02d' $[i+1])
+    new_name="${name%.*}_${num}.${name//*.}"
+
+    ffmpeg -i "$name" -acodec copy -vcodec copy \
+           -t $splitLen -ss $start \
+           -metadata "track=$[i+1]" \
+           "$new_name"
+done
